40dd19ec8f35549c48226309ee82121d1a59ff8e
Federico Agustin Annacondia <fannacondia@emergencias.com.ar>
2021-03-18 10:36:34 -0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,12 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { UserResolver } from "./resolvers/UserResolver";
+import cors from "cors"
 
 (async () => {
   const app = express();
+
+  app.use(cors())
 
   await createConnection();
 
