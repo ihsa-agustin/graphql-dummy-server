21e46da93a5fa9cdc44d58d0a66333dee59d3343
Federico Agustin Annacondia <fannacondia@emergencias.com.ar>
2021-04-20 10:23:27 -0300

    changes

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -24,129 +24,99 @@ class PasswordRecoveryInput {
   password: string;
 }
 
-// @InputType()
-// class UserName {
-//   @Field()
-//   first_name: string;
-
-//   @Field()
-//   last_name: string;
-// }
-
-// @InputType()
-// class Address {
-//   @Field()
-//   province: string;
-
-//   @Field()
-//   location: string;
-
-//   @Field()
-//   neighborhood: string;
-
-//   @Field()
-//   street: string;
-
-//   @Field()
-//   street_number: string;
-
-//   @Field()
-//   floor: string;
-
-//   @Field()
-//   department: string;
-// }
-
-// @InputType()
-// class Phone {
-//   @Field()
-//   type: string;
-
-//   @Field()
-//   number: string;
-// }
-
-// @InputType()
-// class Identities {
-//   @Field()
-//   user_id: string;
-
-//   @Field()
-//   provider: string;
-
-//   @Field()
-//   connection: string;
-// }
-
-
-
 @InputType()
-class RegisterInput {
+class UserName {
   @Field()
-  user_id: number;
-
+  first_name: string
+  
+  @Field()
+  last_name: string
+  
   @Field()
   user_identification: string
   
   @Field()
   user_identification_type: string
-  
-  @Field()
-  user_name: string
-  
-  @Field()
-  email: string
-  
-  @Field()
-  email_verified: boolean
+}
 
+@InputType()
+class SecurityQuestions {
   @Field()
-  address: string
+  id: number
   
+  @Field()
+  value: string
+}
+
+@InputType()
+class Phone {
   @Field()
   phone: string
   
   @Field()
-  is_blocked: boolean
+  mobile_phone: string
   
   @Field()
-  name: string
-
-  @Field()
-  picture: string
-
-  @Field()
-  identities: string
-
-  @Field()
-  created_at: string
+  emergency_phone: string
 }
 
-// interface NameInterface {
-//   first_name: string
-//   last_name: string
-// }
+@InputType()
+class Address {
+  @Field()
+  province: string
+  
+  @Field()
+  location: string
+  
+  @Field()
+  neighborhood: string
+  
+  @Field()
+  street: string
+  
+  @Field()
+  street_number: string
+  
+  @Field()
+  apartment_floor: string
+  
+  @Field()
+  apartment_suit: string
+}
 
-// interface AddressInterface {
-//   province: string
-//   location: string
-//   neighborhood: string
-//   street: string
-//   street_number: string
-//   floor: string
-//   department: string
-// }
 
-// interface PhoneInterface {
-//   type: string
-//   number: string
-// }
+@InputType()
+class RegisterInput {
+  @Field()
+  user_name: UserName
 
-// interface IdentitiesInterface {
-//   user_id: string
-//   provider: string
-//   connection: string
-// }
+  @Field()
+  health_insurance_name: String
+
+  @Field()
+  health_insurance_id: String
+
+  @Field()
+  email: String
+
+  @Field()
+  password: String
+
+  @Field()
+  repeat_password: String
+
+  @Field()
+  phone: Phone
+
+  @Field()
+  address: Address
+
+  @Field()
+  security_questions: [SecurityQuestions]
+
+  @Field()
+  birth_date: String
+}
 
 @ObjectType()
 export class Profiler {
@@ -224,7 +194,21 @@ export class UserResolver {
   }
   @Mutation(() => User)
   async register(@Arg("data", () => RegisterInput) data: RegisterInput) {
-    const user = await User.create(data).save();
+    const newUser = {
+      user_identification: data.user_name.user_identification,
+      user_identification_type: data.user_name.user_identification_type,
+      user_name: JSON.stringify(data.user_name),
+      email: data.email.toString(),
+      email_verified: false,
+      address: JSON.stringify(data.address),
+      phone: JSON.stringify(data.phone),
+      is_blocked: false,
+      name: data.user_name.first_name,
+      picture: "",
+      identities: JSON.stringify([]),
+      created_at: Date.now().toString(),
+    }
+    const user = await User.create(newUser).save();
     return user;
   }
 
